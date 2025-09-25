1c2246630db88170dc57e2c9b33e5fc2ca584d66
Sebastian B <sebastian.barzaghi@outlook.com>
2025-09-25 20:21:31 +0200

    FEAT: add html, pdf, pttx formats

---
+++ lessons/01/generate.sh
@@ -0,0 +1,8 @@
+# HTML
+npx @marp-team/marp-cli@latest 01.md -o dhdmch2526_01.html --theme ../../theme.css
+
+# PDF
+npx @marp-team/marp-cli@latest 01.md -o dhdmch2526_01.pdf --theme ../../theme.css --allow-local-files
+
+# PPTX
+npx @marp-team/marp-cli@latest 01.md -o dhdmch2526_01.pptx --theme ../../theme.css --allow-local-files
