3cc4a98831bfafbcad4b52310cfc1f04a8ead449
Sebastian B <sebastian.barzaghi@outlook.com>
2025-10-09 08:58:46 +0200

    FIX: fix files

---
+++ lessons/03/generate.sh
@@ -0,0 +1,5 @@
+# HTML
+npx @marp-team/marp-cli@latest 03.md -o 03.html --theme ../../theme.css
+
+# PPTX
+npx @marp-team/marp-cli@latest 03.md -o 03.pptx --theme ../../theme.css --allow-local-files
