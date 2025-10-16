4ade4c447df70d6733639ee5252c72d2b8bdcef8
Sebastian B <sebastian.barzaghi@outlook.com>
2025-10-16 23:50:30 +0200

    FIX: start lesosn 06

---
+++ lessons/05/generate.sh
@@ -0,0 +1,5 @@
+# HTML
+npx @marp-team/marp-cli@latest 05.md -o 05.html --theme ../../theme.css
+
+# PPTX
+npx @marp-team/marp-cli@latest 05.md -o 05.pptx --theme ../../theme.css --allow-local-files
